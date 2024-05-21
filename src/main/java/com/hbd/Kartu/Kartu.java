58f1dbd5ada88aa7a24ccb61f70012801fe056c2
TazakiN <tazkia.nizami@gmail.com>
2024-05-21 19:21:39 +0700

    feat: implment hashCode and equals

--- src/main/java/com/hbd/Kartu/Kartu.java
+++ src/main/java/com/hbd/Kartu/Kartu.java
@@ -4,7 +4,7 @@ public abstract class Kartu {
     private final String nama;
     private String imagePath;
 
-    public Kartu(String nama, String imagePath){
+    public Kartu(String nama, String imagePath) {
         this.nama = nama;
         this.imagePath = imagePath;
     }
@@ -13,7 +13,33 @@ public abstract class Kartu {
         return this.nama;
     }
 
-    public String getImagePath(){
+    public String getImagePath() {
         return this.imagePath;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nama == null) ? 0 : nama.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Kartu other = (Kartu) obj;
+        if (nama == null) {
+            if (other.nama != null)
+                return false;
+        } else if (!nama.equals(other.nama))
+            return false;
+        return true;
+    }
+
 }
