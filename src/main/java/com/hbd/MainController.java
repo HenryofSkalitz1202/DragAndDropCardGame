fbb2fd6069bcc7e5fe84ab364d98eef6a8fa2fd9
dhiabziz <dhiditabdi@gmail.com>
2024-05-23 22:57:22 +0700

    fix: button

--- src/main/java/com/hbd/MainController.java
+++ src/main/java/com/hbd/MainController.java
@@ -11,13 +11,61 @@ public class MainController {
     private BorderPane home;
 
     @FXML
-    private void handleButton(ActionEvent event){
-        System.out.println("WAKWAW");
+    private void ButtonLadangku(ActionEvent event){
+        System.out.println("Test1");
+
+        //Kode di bawah ini untuk mengganti view dari bagian tengah halaman
+        //Ganti "testing" dengan nama file fxml asli, misal "ladangku"
         FxmlLoader object = new FxmlLoader();
-        
         Pane view = object.getPage("testing");
         home.setCenter(view);
     }
     
-    
+    @FXML
+    private void ButtonLadangLawan(ActionEvent event){
+        System.out.println("Test2");
+
+        //Kode di bawah ini untuk mengganti view dari bagian tengah halaman
+        //Ganti "testing" dengan nama file fxml asli, misal "ladangku"
+        FxmlLoader object = new FxmlLoader();
+        Pane view = object.getPage("draggable");
+        home.setCenter(view);
+
+    }
+
+    @FXML
+    private void ButtonToko(ActionEvent event){
+        System.out.println("Test3");
+
+        //Kode di bawah ini untuk mengganti view dari bagian tengah halaman
+        //Ganti "testing" dengan nama file fxml asli, misal "ladangku"
+        FxmlLoader object = new FxmlLoader();
+        Pane view = object.getPage("testing");
+        home.setCenter(view);
+
+    }
+
+    @FXML
+    private void ButtonSaveState(ActionEvent event){
+        System.out.println("Test4");
+
+    }
+
+    @FXML
+    private void ButtonLoadPlugin(ActionEvent event){
+        System.out.println("Test5");
+
+    }
+
+    @FXML
+    private void ButtonLoadState(ActionEvent event){
+        System.out.println("Test6");
+
+    }
+
+    @FXML
+    private void ButtonNext(ActionEvent event){
+        System.out.println("Test7");
+
+    }
 }
